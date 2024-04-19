5d3c1b6a7d963a8083b3c966284421191c027689
alexTamilouski <aliaksei.tamilouski@softensity.com>
2024-04-19 14:38:41 +0200

    add posts pages

---
+++ src/components/react/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import styles from "./Footer.module.css";
+
+function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <p>© Alex Tamilouski 2024.&nbsp; This footer is a react component</p>
+    </footer>
+  );
+}
+
+export default Footer;
